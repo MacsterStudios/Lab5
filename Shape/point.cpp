3da43764364745f8bfcf99ee1e43f79c44db017b
MacsterStudios <geremy.sorlini.protic@gmail.com>
2015-03-11 19:59:01 -0400

    Progress for "Point" unit tests
    given_point_when_x_is_negative_then_should_throw_excpetion,
    given_point_when_y_is_negative_then_should_throw_excpetion

--- Shape/point.cpp
+++ Shape/point.cpp
@@ -9,7 +9,8 @@ Point::Point( double _x, double _y ) :
 	x(_x), 
 	y(_y)
 {	
-	
+	if (_x < 0) throw std::invalid_argument("The value for 'X' cannot be negative.");
+	if (_y < 0) throw std::invalid_argument("The value for 'Y' cannot be negative.");
 }
 
 bool Point::operator == (const Point & point) const
