47da161cd7ddef36d8d32e602559977c9c3059b3
MacsterStudios <geremy.sorlini.protic@gmail.com>
2015-03-23 18:15:55 -0400

    Progress for "Rectangle" unit tests
    draw_rectangle_with_fill_color_should_fill_the_rectangle

--- Shape/rectangle.cpp
+++ Shape/rectangle.cpp
@@ -7,6 +7,7 @@ Rectangle::Rectangle(IWindowAPI& _API){
 	position = new Point();
 	width = 0;
 	height = 0;
+	fillSet = false;
 }
 
 void Rectangle::setPosition(Point& _point){
@@ -26,9 +27,20 @@ void Rectangle::draw(){
 	if (height < 0) throw std::invalid_argument("Height cannot be negative.");
 	Color* color = new Color();
 	API->setDrawingColor(*color);
+	
 	API->drawRectangle(*position, height, width);
 	//API->drawLine(*position, Point(position->x+width, position->y));
 	//API->drawLine(*position, Point(position->x, position->y+height));
 	//API->drawLine(Point(position->x, position->y + height), Point(position->x+width, position->y + height));
 	//API->drawLine(Point(position->x+width, position->y), Point(position->x + width, position->y + height));
+	if (fillSet){
+		API->setDrawingColor(fillColor);
+		API->fillRectangle(*position, height, width);
+	}
+}
+
+
+void Rectangle::setFillColor(Color _color){
+	fillSet = true;
+	fillColor = _color;
 }
--- Shape/rectangle.h
+++ Shape/rectangle.h
@@ -11,8 +11,11 @@ namespace ShapeLibrary
 		void setHeight(int _height);
 		void setWidth(int _width);
 		void draw();
+		void setFillColor(Color _color);
 
 	private:
+		bool fillSet;
+		Color fillColor;
 		IWindowAPI* API;
 		Point* position;
 		int height;
